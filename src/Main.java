ce34480b09c609871bf6ae06ed2aaf44605dbf64
Paulo Freitas <paulo.freitas.nt@gmail.com>
2026-08-31 08:59:03 -0300

    Criando versão inicial do programa estruturado

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,16 @@
-void main() {
+import java.util.Scanner;
 
+void main() {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Informe o código:");
+    int codigo = scanner.nextInt();
+    scanner.nextLine();
+    System.out.println("Informe a descrição:");
+    String descricao = scanner.nextLine();
+    System.out.println("Informe o preço:");
+    float preco = scanner.nextFloat();
+    scanner.nextLine();
+    System.out.println("Informe o estoque:");
+    int estoque = scanner.nextInt();
+    scanner.nextLine();
 }
