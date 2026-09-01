1d00e178e020e2f0f94b74352c92fe4d80ebca51
Paulo Freitas <paulo.freitas.nt@gmail.com>
2026-09-01 08:07:49 -0300

    criando construtor e encapsulando

--- src/Produto.java
+++ src/Produto.java
@@ -1,8 +1,18 @@
 public class Produto {
 
-    int codigo;
-    String descricao;
-    float preco;
-    int estoque;
+    private int codigo;
+    private String descricao;
+    private float preco;
+    private int estoque;
+
+    public Produto(int codigo, String descricao,
+                   float preco, int estoque) {
+        this.codigo = codigo;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.estoque = estoque;
+    }
+    //Sobrecarga do operador Produto
+    public Produto(){ }
 
 }
