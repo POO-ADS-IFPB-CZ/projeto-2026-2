f4722c43dac1688f22a83845661e777a6c666984
Paulo Freitas <paulo.freitas.nt@gmail.com>
2026-09-22 08:36:33 -0300

    Criando construtor, getter e setter de loja

--- src/model/Loja.java
+++ src/model/Loja.java
@@ -5,5 +5,36 @@ public class Loja {
     private String cnpj;
     private String nome;
     private Cliente[] clientes;
+    private Produto[] produtos;
 
+    public Loja(String cnpj, String nome){
+        this.cnpj = cnpj;
+        this.nome = nome;
+        clientes = new Cliente[100];
+        produtos = new Produto[100];
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Cliente[] getClientes() {
+        return clientes;
+    }
+
+    public Produto[] getProdutos() {
+        return produtos;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
---
+++ src/model/Produto.java
@@ -0,0 +1,37 @@
+package model;
+
+public class Produto {
+    private int codigo;
+    private String nome;
+    private float preco;
+
+    public Produto(int codigo, String nome, float preco) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+}
